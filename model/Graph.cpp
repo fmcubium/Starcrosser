df0c0c0fdbae6a2bbad4672cc1440654d08415fc
Andres D <84483578+fmcubium@users.noreply.github.com>
2024-12-01 23:59:08 -0500

    Created graph class declaration

---
+++ model/Graph.cpp
@@ -0,0 +1,12 @@
+#include "Graph.h"
+
+float Graph::distGalactic(unsigned long s1, unsigned long s2) {
+
+}
+
+// constructor impls
+
+Graph::Graph() {
+	//load up all the vertices from the data
+
+}
